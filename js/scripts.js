26943352543fbbc99c698ed6a5ae2d583e5bc836
Piotr Madej <piotr.madejkr@gmail.com>
2018-03-18 16:16:16 +0100

    aktualizacja

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
 $(function(){
     console.log('działa!');
-    var carouselList = $("#carousel ul");
-    setInterval(changeSlide, 3000).event(changeSlide () {
-        carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide)});
-    
+var carouselList = $("#carousel ul"); 
+function movefirstslide () {
+    var firstItem = carouselList.find("li:first");
+    var lastItem = carouselList.find("li:last");
+};
+$('#carousel').animate({'marginLeft':'-400'},500,movefirstslide);
+$(carouselList).setInterval(changeSlide, 3000);
+
+
 });
+
